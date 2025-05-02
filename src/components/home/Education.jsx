f4de22bf506f73946e725a637b2e213f3d68a7f3
Aarishshahnawaz <aarishaps2003@gmail.com>
2025-05-02 12:55:28 +0530

    aarish

--- src/components/home/Education.jsx
+++ src/components/home/Education.jsx
@@ -3,7 +3,7 @@ import { GraduationCap, MapPin, Calendar, BookOpen } from "lucide-react";
 
 const educationData = [
   {
-    degree: "Computer Science and enginee ",
+    degree: "Computer Sciences and enginee ",
     institution: "Lovely Professional University",
     location: "Phagwara, Punjab",
     period: "2022 – 2026",
